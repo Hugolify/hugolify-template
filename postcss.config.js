0722077f6dbf62a8a9d62df51db94a4e730dd9a0
sebousan <sebousan@gmail.com>
2022-11-25 15:25:13 +0100

    theme

--- postcss.config.js
+++ postcss.config.js
@@ -18,6 +18,7 @@ module.exports = {
                     /^js-/
                 ],
                 greedy: [
+                    /^aria-current$/,
                     /^role$/
                 ]
             },
